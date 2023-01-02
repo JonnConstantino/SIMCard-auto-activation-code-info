494c30d49a933acce7ec902688dcb57a000fae56
khomp <khomp@ubuntu.myguest.virtualbox.org>
2023-01-02 16:14:28 -0300

    Atualizado auto.py para gerar 2 arquivos prontos para importação na eNB banda 28

--- auto.py
+++ auto.py
@@ -5,18 +5,40 @@
 
 import argparse
 
+UL_DL_AMBR = '50000000'
+APN = 'APNNAME1'
+DEFAULT = 'yes'
+GWIP = '127.0.0.1'
+QCI = '9'
+ARPPRIORLEV = '14'
+ARPPCI = 'on'
+ARPPVI = 'off'
+PDNTYPE = 'ipv4'
+SERVEDPARTYIPV4ADDR = '172.16.0.35'
+SERVEDPARTYIPV6ADDRPREFIX = '1111'
+PRIDNSIPADDR = '8.8.8.8'
+SECONDARYDNSIPADDR = '114.114.114.114'
+
 parser = argparse.ArgumentParser()
 parser.add_argument('arq_simcard', help='Arquivo onde está os IMSI dos SIMCards que deseja ativar.')
 parser.add_argument('arq_ativacao', help='Arquivo onde está os IMSI dos SIMCards com os códigos KI e OPC.')
 args = parser.parse_args()
 
-f_novo = open(args.arq_simcard.replace('.csv', '_ativado.csv'), 'w')
+f_novo_apn = open('apn_b28_' + args.arq_simcard, 'w')
+f_novo_halob = open('halob_b28_' + args.arq_simcard, 'w')
 f_simcard = open(args.arq_simcard, 'r')
 
-f_novo.write('IMSI,KI,OPC,ACTIV_CODE\n')
+f_novo_halob.write("IMSI,IMSIID(no-repeat),UEAMBRDL(bps),UEAMBRUL(bps),KI,OPC,ACTIV_CODE\n")
+f_novo_apn.write("IMSI,APNNAME,CONTEXTID(The same user value is different),DEFAULTAPN(There's only one default for each user),GWIP,QCI(5-9),ARPPRIORLEV,ARPPCI,ARPPVI,APNAMBRUL(bps,1000-1000000000),APNAMBRDL(bps,1000-1000000000),PDNTYPE,SERVEDPARTYIPV4ADDR,SERVEDPARTYIPV6ADDRPREFIX,PRIDNSIPADDR,SECONDARYDNSIPADDR\n")
 
+count = 0
 for line_simcard in f_simcard:
     f_gemalto = open(args.arq_ativacao, 'r')
     for line_gemalto in f_gemalto:
         if line_simcard.strip('\n') in line_gemalto:
-            f_novo.write(line_gemalto)
+            count += 1
+            line_halob = line_gemalto.replace(line_simcard.strip('\n'), line_simcard.strip('\n') + ',' + str(count) + ',' + str(UL_DL_AMBR) + ',' + str(UL_DL_AMBR))
+            line_apn = line_simcard.strip('\n') + ',' + APN + ',' + str(count) + ',' + DEFAULT + ',' + GWIP + ',' + QCI + ',' + ARPPRIORLEV + ',' + ARPPCI + ',' + ARPPVI + ',' + UL_DL_AMBR + ',' + UL_DL_AMBR + ',' + PDNTYPE + ',' + SERVEDPARTYIPV4ADDR.replace('35', str(34+count)) + ',' + SERVEDPARTYIPV6ADDRPREFIX + ',' + PRIDNSIPADDR + ',' + SECONDARYDNSIPADDR + '\n'
+            print(line_apn)
+            f_novo_halob.write(line_halob)
+            f_novo_apn.write(line_apn)
